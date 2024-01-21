dcb845e8e661ab4224f85c4c96761cb55f385163
Tore Aurstad <tore.aurstad@gmail.com>
2024-01-21 23:13:19 +0100

    init commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AzureSpellCheckDemo;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AzureSpellCheckDemo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
